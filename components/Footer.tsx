3d925ac64549361a0b1fb9d8df950f00d2b1e9d0
mrVers <info@vers.si>
2022-01-05 17:34:33 +0100

    Added dependencies and index.tsx refactor

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer-section ">
+    <div className="container">
+      <div className="row">
+
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
