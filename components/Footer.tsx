c8d04c0269d67523dc564afc52cabc9151049f99
mrVers <info@vers.si>
2022-01-11 18:45:40 +0100

    Footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="footer-section ">
+  <footer className="footer-section mb-3">
     <div className="container">
       <div className="row">
-
+        <ul>
+          <li>
+            (c) {(new Date).getFullYear()} Sway Social
+          </li>
+          <li>
+            <a href="https://swaysocial.org" rel="noreferrer" target="_blank">
+              https://swaysocial.org
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/swayprotocol" rel="noreferrer" target="_blank">
+              Github
+            </a>
+          </li>
+          <li>
+            <a href="https://t.me/cloutdotart" rel="noreferrer" target="_blank">
+              Telegram
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/swayprotocol" rel="noreferrer" target="_blank">
+              Twitter
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </footer>
