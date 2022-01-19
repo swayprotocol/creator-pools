57a32b7fe153bf759c2bbe2fe1fa87b0cc3a2531
Matjaz Grosek <matjaz@grosek.si>
2022-01-19 15:53:50 +0100

    updated links for get sway

--- components/Stakes/index.tsx
+++ components/Stakes/index.tsx
@@ -128,7 +128,7 @@ const Stakes: FC<StakesType> = (props: StakesType) => {
               )) : (
                 <div className={styles.inactive}>
                   <p className="mb-4">No active positions yet. <a className="btn-link" onClick={() => props.openModal({ type: ModalType.STAKE })}>Click here</a> to get started.</p>
-                  <p className="mb-0">You will need SWAY to get started. <a target="_blank" rel="noopener noreferrer" href="https://www.gate.io/trade/SWAY_USDT">Get it now</a>.</p>
+                  <p className="mb-0">You will need SWAY to get started. <a target="_blank" rel="noopener noreferrer" href="https://quickswap.exchange/#/swap?outputCurrency=0x262b8aa7542004f023b0eb02bc6b96350a02b728">Get it now</a>.</p>
                 </div>
               )}
             </div>
