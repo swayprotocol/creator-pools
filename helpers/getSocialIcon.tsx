5397070c9cbe037144f6c01ff5d0a50879403f2f
mrVers <info@vers.si>
2022-01-27 13:50:49 +0100

    Fix svg stroke

--- helpers/getSocialIcon.tsx
+++ helpers/getSocialIcon.tsx
@@ -25,8 +25,8 @@ export const getSocialIcon = (social: StakedEventSocialType) => {
   } else if (social === StakedEventSocialType.W) {
     return (
       <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="22" height="22">
-        <path fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2.7" d="M12 24L26 2 40 24 26 32zM26 32L26 2"/>
-        <path fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2.7" d="M12 29L26 37 40 29 26 48zM26 48L26 37M12 24L26 18 40 24"/>
+        <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2.7" d="M12 24L26 2 40 24 26 32zM26 32L26 2"/>
+        <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2.7" d="M12 29L26 37 40 29 26 48zM26 48L26 37M12 24L26 18 40 24"/>
       </svg>
     )
   }
