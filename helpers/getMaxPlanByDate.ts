ef4656f556a68974c5400cb466c09b25ca9f7d7d
Miha Žnidar <mayki.znidar@gmail.com>
2022-07-08 14:51:01 +0100

    getMaxPlan function fix

--- helpers/getMaxPlanByDate.ts
+++ helpers/getMaxPlanByDate.ts
@@ -1,6 +1,6 @@
 import { Plan } from '../shared/interfaces';
 
 export const getMaxPlanByDate = (stakedAt: Date, plans: Plan[]): Plan => {
-  const possiblePlans = plans.filter(plan => (+plan.availableUntil > +stakedAt) && ((+plan.availableUntil - plan.lockMonths*30*24*60*60*1000) < +stakedAt));
+  const possiblePlans = plans.filter(plan => (+plan.availableUntil > +stakedAt) && ((+plan.availableUntil - plan.lockMonths*31*24*60*60*1000) < +stakedAt));
   return possiblePlans.reduce((prev, current)=> prev.apy > current.apy ? prev : current);
 }
